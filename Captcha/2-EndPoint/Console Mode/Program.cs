3fc69a8cc1098611d1acc18938f1d28b401e1be2
A.Bagheri <A.Bagheri@BAGHERI>
2021-09-12 12:18:48 +0430

    fix

--- Captcha/2-EndPoint/Console Mode/Program.cs
+++ Captcha/2-EndPoint/Console Mode/Program.cs
@@ -1,25 +1,57 @@
-﻿namespace Captcha
+﻿using System;
+namespace Captcha
 {
     class Program
     {
         static void Main(string[] args)
         {
+            #region Test Create Captcha
+
             var captchaManager = new CaptchaManager();
 
             var generateCaptchaResponse = captchaManager.GenerateCaptcha();
 
-            var captcha= generateCaptchaResponse.Captcha;
+            var captcha = generateCaptchaResponse.Captcha;
+
+            Console.WriteLine($"Captcha code is :{captcha}");
 
             var captchaContent = generateCaptchaResponse.CaptchaContent;
 
             var captchaId = generateCaptchaResponse.CaptchaId;
 
-            //
-            string loginCaptcha=captcha;
+            #endregion
 
-            var verifyCaptchaRequest = new VerifyCaptchaRequest() {Captcha = captcha, LoginCaptcha = loginCaptcha};
+            #region Test Verify Captcha
 
-            var verifyCaptchaResponse= captchaManager.VerifyCaptcha(verifyCaptchaRequest);
+            Console.WriteLine("please inter the security code(Captcha Code)");
+            string inputCaptcha = Console.ReadLine();
+
+            var verifyCaptchaRequest = new VerifyCaptchaRequest() { Captcha = captcha, LoginCaptcha = inputCaptcha };
+
+            var verifyCaptchaResponse = captchaManager.VerifyCaptcha(verifyCaptchaRequest);
+
+            switch (verifyCaptchaResponse.Result.ActionResult)
+            {
+                case eVerifyCaptchaResponse.Matched:
+                    Console.WriteLine("Matched");
+                    break;
+                case eVerifyCaptchaResponse.Expired:
+                    Console.WriteLine("Expired");
+                    break;
+                case eVerifyCaptchaResponse.NotMatched:
+                    Console.WriteLine("NotMatched");
+                    break;
+                case eVerifyCaptchaResponse.TryAgain:
+                    Console.WriteLine("TryAgain");
+                    break;
+                default:
+                    Console.WriteLine("TryAgain");
+                    break;
+            }
+            
+            #endregion
+
+            Console.ReadKey();
         }
     }
 }
